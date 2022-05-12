5467a3b06af7f471d0a9a1a7a920efdb83b2d071
Justin Findlay <justin@findlay.space>
2022-05-11 23:12:03 -0400

    Rename to tetratile; refactor types and classes

--- test/test_pysirtet.py
+++ test/test_pysirtet.py
@@ -1,22 +1,31 @@
-# -*- coding: utf-8 -*-
-
-# Import python libs
+import functools
 import random
 import pytest
+from decimal import Decimal as D
 from contextlib import contextmanager
-
-# Import pysirtet libs
-from pysirtet.pysirtet import Polyomino, Grid, Transformation, Config
-from pysirtet.pysirtet import tetrominoes
+from typing import Generator, Mapping
+from tetratile.tetratile import Config, Dim, Grid, Polyomino, Translation, Rotation, tetrominoes
 
 
 # https://stackoverflow.com/a/42327075
 @contextmanager
-def not_raises(exception):
+def not_raises(exception:Exception)->Generator[None, None, None]:
     try:
         yield
     except exception:
         raise pytest.fail('DID RAISE {}'.format(exception))
+
+
+@functools.cache
+def grid_superset(width:int, height:int):
+    '''
+    Generate an arbitrary covering, $(w + h)^2$, of the grid.  I could try to
+    work this into a `pytest.fixture`, but do not know how to pass args to
+    fixtures, and this function does what is needed.
+    '''
+    x_out:List[int] = [i for i in range(-height//2, -1)] + [i for i in range(width, width + height//2)]
+    y_out:List[int] = [j for j in range(-width//2, -1)] + [j for j in range(height, height + width//2)]
+    return (x_out, y_out)
 
 
 class TestPolyomino:
@@ -24,12 +33,12 @@ class TestPolyomino:
     Test the Polyomino class
     '''
     @pytest.fixture(autouse=True)
-    def setup_grid(self):
+    def setup_grid(self)->None:
         '''
         Setup program options and grid state
         '''
-        self.opts = Config(test=True).get_opts()
-        self.grid = Grid(self.opts, self.opts['board']['width'], self.opts['board']['height'])
+        self.opts:Mapping = Config(test=True).get_opts()
+        self.grid:Grid = Grid(self.opts, self.opts['board']['width'], self.opts['board']['height'])
 
     def test__check_grid(self):
         '''
@@ -41,13 +50,12 @@ class TestPolyomino:
 
         # Test that interior squares that are occupied are invalid
         for i in range(random.randint(min(self.grid.width, self.grid.height), self.grid.width*self.grid.height)):
-            self.grid[random.randint(0, self.grid.width - 1), random.randint(0, self.grid.height - 1)]['type'] = random.choice(tetrominoes).name
+            self.grid[random.randint(0, self.grid.width - 1), random.randint(0, self.grid.height - 1)]['type']:str = random.choice(tetrominoes).name
         for v, square in self.grid:
             assert (True if self.grid[v]['type'] == None else False) == Polyomino._check_grid(Polyomino, [v], self.grid)
 
-        # Test that exterior squares are invalid (pick an arbitrary covering, $(wh)^2$, of the grid)
-        x_out = [i for i in range(-self.grid.height//2, -1)] + [i for i in range(self.grid.width, self.grid.width + self.grid.height//2)]
-        y_out = [j for j in range(-self.grid.width//2, -1)] + [j for j in range(self.grid.height, self.grid.height + self.grid.width//2)]
+        # Test that exterior squares are invalid (pick an arbitrary covering, $(w + h)^2$, of the grid)
+        (x_out, y_out) = grid_superset(self.grid.width, self.grid.height)
         for x in x_out:
             for y in y_out:
                 assert False == Polyomino._check_grid(Polyomino, [(x, y)], self.grid)
@@ -63,24 +71,24 @@ class TestPolyomino:
         Test the minimum coordinate function
         '''
         # Setup random 'polyomino'
-        ordinal = random.randint(2, 11)
-        coords = [[random.randint(0, self.grid.height) for i in range(2)] for j in range(ordinal)]
-        p = Polyomino(None, None, coords)
+        ordinal:int = random.randint(2, 11)
+        coords:List[List[Tuple[int, int]]] = [[random.randint(0, self.grid.height) for i in range(2)] for j in range(ordinal)]
+        p:Polyomino = Polyomino(None, None, coords)
 
-        for i in range(2):
-            assert p.min(i) == min([c[i] for c in coords])
+        for dim in (Dim.x, Dim.y):
+            assert p.min(dim) == min([c[dim.value] for c in coords])
 
     def test_max(self):
         '''
         Test the maximum coordinate function
         '''
         # Setup random 'polyomino'
-        ordinal = random.randint(2, 11)
-        coords = [[random.randint(0, self.grid.height) for i in range(2)] for j in range(ordinal)]
-        p = Polyomino(None, None, coords)
+        ordinal:int = random.randint(2, 11)
+        coords:List[List[Tuple[int, int]]] = [[random.randint(0, self.grid.height) for i in range(2)] for j in range(ordinal)]
+        p:Polyomino = Polyomino(None, None, coords)
 
-        for i in range(2):
-            assert p.max(i) == max([c[i] for c in coords])
+        for dim in (Dim.x, Dim.y):
+            assert p.max(dim) == max([c[dim.value] for c in coords])
 
     def test_rotate(self):
         '''
@@ -89,15 +97,15 @@ class TestPolyomino:
         # For each tetromino
         for tetromino in tetrominoes:
             # Test each direction
-            for direction in (Transformation.cw, Transformation.ccw):
+            for direction in (Rotation.cw, Rotation.ccw):
                 # Move piece to center of grid
                 tetromino.translate([self.grid.width//2, self.grid.height//2], self.grid)
-                rotation_states = {}
+                rotation_states:Mapping = {}
                 # And cover the rotation space twice
                 for quarter in range(9):
                     # Test that rotation is successful
-                    assert True == tetromino.rotate(Transformation.cw, self.grid)
-                    rotation_states[quarter] = tetromino.coords
+                    assert tetromino.rotate(Rotation.cw, self.grid)
+                    rotation_states[quarter]:List[Tuple[D, D]] = tetromino.coords
                     if quarter - 4 in rotation_states:
                         # Test that rotations are isometric mod 4
                         assert rotation_states[quarter] == rotation_states[quarter - 4]
@@ -116,22 +124,22 @@ class TestPolyomino:
                 assert True == tetromino.translate(translation, self.grid)
 
             # Test each extremal translation
-            for translation in (Transformation.min, Transformation.max, Transformation.bottom):
+            for translation in (Translation.min, Translation.max, Translation.bottom):
                 # Move piece to center of grid
                 tetromino.translate([self.grid.width//2 - int(tetromino.o[0]), self.grid.height//2 - int(tetromino.o[1])], self.grid)
                 # Test that translation is successful
                 assert True == tetromino.translate(translation, self.grid)
                 # Test that translation is idempotent
-                coords = tetromino.coords
+                coords:List[Tuple[D, D]] = tetromino.coords
                 tetromino.translate(translation, self.grid)
                 assert coords == tetromino.coords
                 # Test that translation is extremal
-                if translation == Transformation.min:
-                    assert 0 == tetromino.min(0)
-                if translation == Transformation.max:
-                    assert self.grid.width == tetromino.max(0) + 1
-                if translation == Transformation.bottom:
-                    assert 0 == tetromino.min(1)
+                if translation == Translation.min:
+                    assert 0 == tetromino.min(Dim.x)
+                if translation == Translation.max:
+                    assert self.grid.width == tetromino.max(Dim.x) + 1
+                if translation == Translation.bottom:
+                    assert 0 == tetromino.min(Dim.y)
 
 
 class TestGrid:
@@ -143,8 +151,8 @@ class TestGrid:
         '''
         Setup program options and create a test grid instance
         '''
-        self.opts = Config(test=True).get_opts()
-        self.grid = Grid(self.opts, self.opts['board']['width'], self.opts['board']['height'])
+        self.opts:Mapping = Config(test=True).get_opts()
+        self.grid:Grid = Grid(self.opts, self.opts['board']['width'], self.opts['board']['height'])
 
     def test_get(self):
         '''
@@ -155,9 +163,8 @@ class TestGrid:
             with not_raises(IndexError) as e_info:
                 assert square['type'] == None
 
-        # Test that exterior squares are invalid (pick an arbitrary covering, $(wh)^2$, of the grid)
-        x_out = [i for i in range(-self.grid.height//2, -1)] + [i for i in range(self.grid.width, self.grid.width + self.grid.height//2)]
-        y_out = [j for j in range(-self.grid.width//2, -1)] + [j for j in range(self.grid.height, self.grid.height + self.grid.width//2)]
+        # Test that exterior squares are invalid (pick an arbitrary covering, $(w + h)^2$, of the grid)
+        (x_out, y_out) = grid_superset(self.grid.width, self.grid.height)
         for x in x_out:
             for y in y_out:
                 with pytest.raises(IndexError) as e_info:
@@ -179,22 +186,21 @@ class TestGrid:
         for i in range(self.grid.width):
             for j in range(self.grid.height):
                 with not_raises(IndexError) as e_info:
-                    tetromino_name = random.choice(tetrominoes).name
-                    self.grid[[i, j]]['type'] = tetromino_name
+                    tetromino_name:str = random.choice(tetrominoes).name
+                    self.grid[[i, j]]['type']:str = tetromino_name
                     assert self.grid[[i, j]]['type'] == tetromino_name
 
-        # Test that exterior squares are invalid (pick an arbitrary covering, $(wh)^2$, of the grid)
-        x_out = [i for i in range(-self.grid.height//2, -1)] + [i for i in range(self.grid.width, self.grid.width + self.grid.height//2)]
-        y_out = [j for j in range(-self.grid.width//2, -1)] + [j for j in range(self.grid.height, self.grid.height + self.grid.width//2)]
+        # Test that exterior squares are invalid (pick an arbitrary covering, $(w + h)^2$, of the grid)
+        (x_out, y_out) = grid_superset(self.grid.width, self.grid.height)
         for x in x_out:
             for y in y_out:
                 with pytest.raises(IndexError) as e_info:
-                    self.grid[[x, y]] = random.choice(tetrominoes).name
+                    self.grid[[x, y]]:str = random.choice(tetrominoes).name
         for x in range(self.grid.width):
             for y in y_out:
                 with pytest.raises(IndexError) as e_info:
-                    self.grid[[x, y]] = random.choice(tetrominoes).name
+                    self.grid[[x, y]]:str = random.choice(tetrominoes).name
         for x in x_out:
             for y in range(self.grid.height):
                 with pytest.raises(IndexError) as e_info:
-                    self.grid[[x, y]] = random.choice(tetrominoes).name
+                    self.grid[[x, y]]:str = random.choice(tetrominoes).name
